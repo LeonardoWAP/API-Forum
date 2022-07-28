9043e9d26e92961b4317a40ae98ecbf2b307acb2
LeonardoWAP <leonardowillersap@gmail.com>
2022-07-28 09:34:14 -0300

    rising spring initializer with the libraries that will be used

---
+++ ForumAPI (1)/ForumAPI/src/test/kotlin/com/ForumApi/ForumApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ForumApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
