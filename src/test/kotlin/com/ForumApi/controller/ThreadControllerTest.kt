56ca5b67c88ff32d80ceb9cd0e9b0cd33acf34be
LeonardoWAP <leonardowillersap@gmail.com>
2022-08-04 11:32:43 -0300

    add creating thread Test

---
+++ src/test/kotlin/com/ForumApi/controller/ThreadControllerTest.kt
@@ -0,0 +1,59 @@
+package com.ForumApi.controller
+
+import com.ForumApi.Enums.ThreadStatus
+import com.ForumApi.controller.request.PostThreadRequest
+import com.ForumApi.repository.CustomerRepository
+import com.ForumApi.repository.ThreadRepository
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import kotlin.concurrent.thread
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration
+class ThreadControllerTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var threadRepository: ThreadRepository
+
+    @Autowired
+    private lateinit var objectMapper : ObjectMapper
+
+    @BeforeEach
+    fun setup() = threadRepository .deleteAll()
+
+    @Test
+    fun `should create thread`(){
+        val request = PostThreadRequest(
+            "oi",
+            ThreadStatus.ABERTO,
+            "oi"
+        )
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/threads")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)
+                )
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+        val threads = threadRepository.findAll().toList()
+        Assertions.assertEquals(1, threads.size)
+        Assertions.assertEquals(request.status, threads.first().status)
+        Assertions.assertEquals(request.description, threads.first().description)
+        Assertions.assertEquals(request.title, threads.first().title)
+
+    }
+}
+
